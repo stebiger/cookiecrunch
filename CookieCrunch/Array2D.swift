9bd9dceb81ad1667477651f38d28f60fe893b12c
Stefan Billeb <stefan.billeb@gmx.net>
2016-01-02 16:22:35 +0100

    Add generic Array2D

---
+++ CookieCrunch/Array2D.swift
@@ -0,0 +1,27 @@
+//
+//  Array2D.swift
+//  CookieCrunch
+//
+//  Created by Stefan on 02.01.16.
+//  Copyright © 2016 Stefan Billeb. All rights reserved.
+//
+
+struct Array2D<T> {
+    let columns: Int
+    let rows: Int
+    private var array: Array<T?>
+    
+    init(columns: Int, rows: Int) {
+        self.columns = columns
+        self.rows = rows
+    }
+    
+    subscript(column: Int, row: Int) -> T? {
+        get {
+            return array[row * columns + column]
+        }
+        set {
+            array[row * columns + column] = newValue
+        }
+    }
+}
