f9f3ac9e724a5cfc3e3a9d67a255439b84c2e635
Stefan Billeb <stefan.billeb@gmx.net>
2016-01-03 14:37:51 +0100

    Add recursive chain removal and new possible swap detection

--- CookieCrunch/GameViewController.swift
+++ CookieCrunch/GameViewController.swift
@@ -51,15 +51,23 @@ class GameViewController: UIViewController {
     func handleMatches() {
         let chains = level.removeMatches()
         
+        if chains.count == 0 {
+            beginNextTurn()
+            return
+        }
         scene.animateMatchedCookies(chains) {
             let columns = self.level.fillHoles()
             self.scene.animateFallingCookies(columns) {
                 let columns = self.level.topUpCookies()
                 self.scene.animateNewCookies(columns) {
-                    self.view.userInteractionEnabled = true
-                }
+                    self.handleMatches()                }
             }
         }
+    }
+    
+    func beginNextTurn() {
+        level.detectPossibleSwaps()
+        view.userInteractionEnabled = true
     }
     
     func beginGame() {
